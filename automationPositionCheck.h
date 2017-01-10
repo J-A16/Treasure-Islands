02d071026795db4e198aab790509e57b48684639
J-A16 <Jeremy.Alexandr@gmail.com>
2017-01-10 00:02:58 -0500

    Create automationPositionCheck.h

---
+++ automationPositionCheck.h
@@ -0,0 +1,10 @@
+#ifndef AUTOMATIONPOSITIONCHECK_H
+#define AUTOMATIONPOSITIONCHECK_H
+
+#include "Board.h"
+
+void automationPositionCheck(Board playerBoard, int &startingPosition,
+			     int treasureSize);
+
+
+#endif
